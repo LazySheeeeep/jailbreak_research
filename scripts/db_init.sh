7df490eac8b29d4f6cdb3cd59838b9bbf5cb7738
root <root@zher>
2024-05-18 16:55:51 +0800

    improve on the jailbreak prompts directory naming rule

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -8,7 +8,7 @@ mysql <$cwd/settings_init.sql
 
 for ((i = 1; i <= 5; i++)); do
 	for ((j = 1; j <= 5; j++)); do
-		prompt_raw=$(cat "$repo_root/jb_prompts/$((i - 1))_$((j - 1)).txt")
+		prompt_raw=$(cat "$repo_root/jb_prompts/${i}_$j.txt")
 		mysql -D jailbreak_research -e "INSERT INTO jailbreak_prompts (jt_id, jp_id, jp) VALUES ($i, $j, '${prompt_raw//\'/\'\'}')"
 	done
 done
