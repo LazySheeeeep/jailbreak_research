be314a641849de9ce49695be0f9126e5a988ee3b
LazySheeeeep <mapleontheviolin@proton.me>
2024-05-26 22:29:01 +0800

    renaming

--- scripts/plot_all_fig.sh
+++ scripts/plot_all_fig.sh
@@ -4,6 +4,7 @@ cwd="$(dirname "$(readlink -f "$0")")"
 repo_root=$cwd/..
 
 for i in {1..8}; do
-    python $repo_root/scripts/analysis_fig_gen.py -mid $i
+    python $repo_root/scripts/per_model_analysis_fig_gen.py -mid $i
 done
-python $repo_root/scripts/analysis_fig_gen.py -mid 10
+
+python $repo_root/scripts/all_results_analysis_fig_gen.py
