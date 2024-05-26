f1480a585ba609b0a0bf866463c2f608ee087ffa
LazySheeeeep <mapleontheviolin@proton.me>
2024-05-26 22:29:49 +0800

    bug fix and add sth in ignore

--- scripts/backdoor_insertion.sh
+++ scripts/backdoor_insertion.sh
@@ -6,15 +6,10 @@ mysqljb() {
 	mysql -D jailbreak_research --skip-column-names --batch -e "$1"
 }
 
-for i in {1..5}; do
+for i in {1..6}; do
     for j in {1..5}; do
-        # Fetch the mq value from the database
         mq=$(mysqljb "select mq from malicious_questions where ps_id=${i} and mq_id=${j}")
-        
-        # Add backdoor flags to the mq value
-        backdoored_mq="${BACKDOOR_FLAG_S}${mq}${BACKDOOR_FLAG_E}"
-        
-        # Insert the backdoored mq value into the database
+        backdoored_mq="${backdoor_flag_s}${mq}${backdoor_flag_e}"
         mysqljb "insert into malicious_questions (ps_id, mq_id, mq) VALUES (${i}, $((j+10)), '${backdoored_mq//\'/\\\'}')"
     done
 done
